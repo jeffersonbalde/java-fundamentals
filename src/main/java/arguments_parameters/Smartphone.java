9badd6ca14e7ccbb2675e67a8f14d1a3d593e767
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-26 22:03:33 +0800

    adding basics

---
+++ src/main/java/arguments_parameters/Smartphone.java
@@ -0,0 +1,11 @@
+package arguments_parameters;
+
+public class Smartphone {
+    double price;
+    String color;
+    String brand;
+
+    public void text() {
+        System.out.println("I CAN TEXT");
+    }
+}
---
+++ src/main/java/arguments_parameters/SmartphoneMain.java
@@ -0,0 +1,4 @@
+package arguments_parameters;
+
+public class SmartphoneMain {
+}
