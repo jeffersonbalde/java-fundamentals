01d9d4c86be0ba829d4c1c59e72a63aecbd33bb4
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-28 07:54:47 +0800

    Implementing rectangle class program

---
+++ src/main/java/java_objects/Rectangle.java
@@ -0,0 +1,53 @@
+package java_objects;
+
+import org.w3c.dom.css.Rect;
+
+import java.util.concurrent.RecursiveAction;
+
+public class Rectangle {
+    private double length;
+    private double width;
+    private int sides = 4;
+
+    public Rectangle() {
+        setLength(0);
+        setWidth(0);
+    }
+
+    public Rectangle(double length, double width) {
+        setLength(length);
+        setWidth(width);
+    }
+
+    public double calculateParameter() {
+        return (2 * length) + (2 * width);
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+}
