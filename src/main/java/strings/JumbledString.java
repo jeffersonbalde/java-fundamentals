714f7de13695601c16d62b97e0788daa651df7d3
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-05-08 09:07:33 +0800

    jumbled string java program

---
+++ src/main/java/strings/JumbledString.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class JumbledString {
+    public static void main(String[] args) {
+        addSpaces("YouWillBecomeSoftwareEngineerSoon");
+    }
+
+    public static void addSpaces(String text) {
+        var modifiedText = new StringBuilder(text);
+        for(int i=0; i<modifiedText.length(); i++) {
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))) {
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
+    }
+}
